084f8434d4659507ed64b9d7592862af41059cde
Dzmitry_Prudnikau <dzmitriyprudnikov@gmail.com>
2018-05-09 15:39:03 +0300

    upgrade homework-2 implementation

--- dirwatcher.js
+++ dirwatcher.js
@@ -4,6 +4,7 @@ import fs from 'fs';
 export default class DirWatcher{
     constructor() {
         this.watchedFiles = [];
+        this.timeFilesWasChanged = [];
     }
 
     watch(path, delay) {
@@ -13,6 +14,15 @@ export default class DirWatcher{
                 if (!this.watchedFiles.includes(file)) {
                     eventEmitter.emit('changed', `${path + file}`);
                     this.watchedFiles.push(file);
+                    this.timeFilesWasChanged.push(fs.statSync(path + file).mtime);
+                } else {
+                    const index = this.watchedFiles.indexOf(file);
+                    const lastTimeFileWasChanged = fs.statSync(path + file).mtime;
+                    if (Date.parse(lastTimeFileWasChanged) !== Date.parse(this.timeFilesWasChanged[index])) {
+                        eventEmitter.emit('changed', `${path + file}`);
+                        this.watchedFiles.splice(index, 1, file);
+                        this.timeFilesWasChanged.splice(index, 1, lastTimeFileWasChanged);
+                    }
                 }
             });
         }, delay);
